e85726d4c5ecbd44d410612890666a1429196603
hkdigiverse2108 <hkdigiversework@gmail.com>
2026-04-04 11:39:30 +0530

    feat: initialize React root with QueryClient, Auth, and Theme providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './Context/AuthContext.tsx'
@@ -17,7 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <App />
         </ThemeProvider>
       </AuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 )
